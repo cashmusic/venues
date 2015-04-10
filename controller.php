69db9d2554f499890c20d844034ab88f166cb6c5
Jesse von Doom <jesse@cashmusic.org>
2015-04-10 14:02:02 -0700

    looking at whitespace issues

--- controller.php
+++ controller.php
@@ -14,8 +14,8 @@ $requested_action = false;
 
 // look for '.html' and set output format to 'html' if found
 if (strpos($route,'.html') !== false) {
-	$output_format = 'html';
-	$route = str_replace('.html','',$route); // correct route by removing the .html after format set
+   $output_format = 'html';
+   $route = str_replace('.html','',$route); // correct route by removing the .html after format set
 
 }
 
@@ -30,14 +30,14 @@ if (strpos($route,'.html') !== false) {
 $exploded_route = explode("/",$route);
 
 if (count($exploded_route) > 1 && $exploded_route[2] == 'edit.php') {
-	$requested_action = 'edited';
+   $requested_action = 'edited';
 } else if (count($exploded_route) > 1 && $exploded_route[1] == 'edit') {
-	$requested_action = 'edit';
+   $requested_action = 'edit';
    $UUID = $exploded_route[2];
 } else if (count($exploded_route) == 1 && $exploded_route[0] == 'venues') {
-	$requested_action = 'search';
+   $requested_action = 'search';
 } else if (count($exploded_route) > 1 && $exploded_route[0] == 'venues') {
-	$requested_action = 'details';
+   $requested_action = 'details';
    $UUID = intval($exploded_route[1]);
 } else {
    $requested_action = 'index';
@@ -54,101 +54,101 @@ require_once('database.php');
 
 // if we found something to do, include needed files and get doing...
 if ($requested_action) {
-	if ($requested_action == 'edited') {
+   if ($requested_action == 'edited') {
       // run through an update statement
-		try {
-			$sql =  "UPDATE venues SET ";
+      try {
+         $sql =  "UPDATE venues SET ";
          $sql .= "name = ?,address1 = ?,address2 = ?,city = ?,region = ?,country = ?,postalcode = ?,url = ?,phone = ?,type = ? ";
          $sql .= "WHERE UUID = ?";
 
-			$q = $db->prepare($sql);
-			$q->execute(array(
+         $q = $db->prepare($sql);
+         $q->execute(array(
             $_POST
          ));
 
-		} catch(Exception $e) {
-			echo $e->getMessage();
-			exit;
+      } catch(Exception $e) {
+         echo $e->getMessage();
+         exit;
 
-		}
+      }
 
       // output content to browser (we're cheating here, assuming success)
       outputContent($_POST,$output_format,'edit');
 
-	} else if ($requested_action == 'edit') {
-		// load the venue with the matching UUID for editing
-		try {
-			$venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
-			$venuearray->bindParam(1, $UUID);
-			$venuearray->execute();
+   } else if ($requested_action == 'edit') {
+      // load the venue with the matching UUID for editing
+      try {
+         $venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
+         $venuearray->bindParam(1, $UUID);
+         $venuearray->execute();
 
-		} catch(Exception $e) {
-			echo $e->getMessage();
-			exit;
-		}
+      } catch(Exception $e) {
+         echo $e->getMessage();
+         exit;
+      }
 
-		// load mustache template for edit page
-		$venue = $venuearray->fetch(PDO::FETCH_ASSOC);
+      // load mustache template for edit page
+      $venue = $venuearray->fetch(PDO::FETCH_ASSOC);
       // output content to browser
       outputContent($venue,$output_format,'edit');
 
-	} else if ($requested_action == 'details') {
-		// load venue with matching UUID, this info is on the specific venue details page
-		try {
-			$venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
-			$venuearray->bindParam(1, $UUID);
-			$venuearray->execute();
+   } else if ($requested_action == 'details') {
+      // load venue with matching UUID, this info is on the specific venue details page
+      try {
+         $venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
+         $venuearray->bindParam(1, $UUID);
+         $venuearray->execute();
 
-		} catch(Exception $e) {
-			echo $e->getMessage();
-			exit;
-		}
+      } catch(Exception $e) {
+         echo $e->getMessage();
+         exit;
+      }
 
-		$venue = $venuearray->fetch(PDO::FETCH_ASSOC);
+      $venue = $venuearray->fetch(PDO::FETCH_ASSOC);
 
-		if ($venue) {
+      if ($venue) {
          // output content to browser
          outputContent($venue,$output_format,'venue');
-		} else {
-			// stuff didn't work!
-			echo "404 not found!";
-		}
+      } else {
+         // stuff didn't work!
+         echo "404 not found!";
+      }
 
-	} elseif ($requested_action == 'search') {
-		$name = $_GET['q']; // the search term
+   } elseif ($requested_action == 'search') {
+      $name = $_GET['q']; // the search term
 
-		if(isset($name)) {
-			// gets all venues with the search term in the name somewhere
-			try {
-				$searcharray = $db->prepare("SELECT * FROM venues WHERE name LIKE '%" . $name . "%'");
-				$searcharray->execute();
-			} catch(Exception $e) {
-				echo $e->getMessage();
-				exit;
-			}
+      if(isset($name)) {
+         // gets all venues with the search term in the name somewhere
+         try {
+            $searcharray = $db->prepare("SELECT * FROM venues WHERE name LIKE '%" . $name . "%'");
+            $searcharray->execute();
+         } catch(Exception $e) {
+            echo $e->getMessage();
+            exit;
+         }
 
-			$search = $searcharray->fetchALL(PDO::FETCH_ASSOC);
+         $search = $searcharray->fetchALL(PDO::FETCH_ASSOC);
          $search_results = array("results" => $search, "name" => $name)
 
          // output content to browser
          outputContent($search_results,$output_format,'search');
-		}
-	}
+      }
+   }
 } else {
 
-	//Index page!
-	// loads ALL venues in the db
-	try {
-		$results = $db->query('SELECT * FROM venues');
-	} catch(Exception $e) {
-		echo $e->getMessage();
-		exit;
-	}
-	$venues = $results->fetchAll(PDO::FETCH_ASSOC);
+   //Index page!
+   // loads ALL venues in the db
+   try {
+      $results = $db->query('SELECT * FROM venues');
+   } catch(Exception $e) {
+      echo $e->getMessage();
+      exit;
+   }
+   $venues = $results->fetchAll(PDO::FETCH_ASSOC);
 
-	// Load mustache template for main page
-	$template = $mustache->loadTemplate('mainpage');
-	echo $template->render(array("results" => $venues));
+   // Load mustache template for main page
+   $template = $mustache->loadTemplate('mainpage');
+   echo $template->render(array("results" => $venues));
 }
 
 
@@ -159,21 +159,21 @@ if ($requested_action) {
 **************************************************************/
 
 function setMustache() {
-	//set up Mustache
-	require_once(__DIR__.'/lib/mustache/Autoloader.php');
-	Mustache_Autoloader::register();
-	$mustache = new Mustache_Engine(array(
-		'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/views')
-	));
+   //set up Mustache
+   require_once(__DIR__.'/lib/mustache/Autoloader.php');
+   Mustache_Autoloader::register();
+   $mustache = new Mustache_Engine(array(
+      'loader' => new Mustache_Loader_FilesystemLoader(__DIR__.'/views')
+   ));
 }
 
 function setHeaders($output_format) {
-	if ($output_format == 'json') {
-		header("Access-Control-Allow-Origin: *");
-		header('Content-type:application/json');
-	} else if ($output_format == 'html') {
-		header('Content-type:text/html; charset=utf-8');
-	}
+   if ($output_format == 'json') {
+      header("Access-Control-Allow-Origin: *");
+      header('Content-type:application/json');
+   } else if ($output_format == 'html') {
+      header('Content-type:text/html; charset=utf-8');
+   }
 }
 
 function outputContent($data,$output_format,$template=false) {
@@ -181,8 +181,8 @@ function outputContent($data,$output_format,$template=false) {
    if ($output_format == 'html') {
       setMustache();
       $template = $mustache->loadTemplate($template);
-   	echo $template->render($data);
-	} else {
+      echo $template->render($data);
+   } else {
       echo json_encode($data);
    }
 }
