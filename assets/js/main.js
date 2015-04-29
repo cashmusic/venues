80abb9e18722f8fa29f3732549717b2a5fa1285c
Chris Leckie <me@christopherleckie.com>
2015-04-29 14:59:49 -0700

    Main JS for Search

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+
+console.log('loaded...');
+  GetURLParameter();
+
+
+/* Input State Change */
+$( "#keyword" ).change(function() {
+
+searchit();
+    
+}); //Input state Change
+
+/* Search Submit */
+$("#search_submission").submit(function(e) {
+    e.preventDefault();
+});
+
+/* Inputting Text */
+$('#keyword').on('input', function() {
+    // do something
+		console.log('typing...');
+		history.pushState({}, '', $(this).val());
+   		return false;
+});
+
+
+function searchit() {
+// If the field isn't empty do a search
+  if ($("#keyword").val() != ''){
+
+    console.log('submitted...');
+
+    //empty the results 
+    $('.results').empty();
+
+    //getJSON
+    $.getJSON( "/venues?q="+$('#keyword').val(), function( data ) {
+
+      // If there are results show them
+      if (data.results != ""){
+
+        var items = [];
+    
+        var ul = $('<section>').addClass('results').appendTo('body');
+          $(data.results).each(function(index, item) {
+            ul.append("<div class='result' id='" + item.id + "'><a class='card' href='/venues/"+item.UUID+".html'><h1>" + item.name +"</h1><span>" + item.UUID + "</span></a></div>")
+          });
+
+        }else{
+          console.log('no joy');
+         $('<section>').addClass('results dice').appendTo('body').html("<div class='dice'><h1>No dice :(</h1></div><!--dice-->");
+        }
+
+    //show results area 
+    $('body').addClass('display');
+
+    }); //getJSON
+  } // If you submitted something
+} //searchit();
+
+function GetURLParameter(sParam)
+  {
+    console.log('getting URL');
+    var sPageURL = window.location.search.substring(1);
+     //console.log(sPageURL);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+
+    }
+  }
+
+
+}); // $document
