bd8657386d7d641d7acd4afdf6c1d1cf62c82552
Chris Leckie <me@christopherleckie.com>
2015-05-04 13:57:30 -0700

    Reset page if input is empty

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,11 @@ $(document).ready(function() {
 var dirs = window.location.pathname.substring(1);
 
 if (dirs == "" ){
-//console.log('loading...');
 $('body').addClass('loading');
 
 setTimeout(function() {
   $('body').removeClass('loading');
   $('body').addClass('loaded');
-  //console.log('loaded...');
-  // get the location
-  //console.log(dirs);
-  
   //run search
   searchit();     
 }, 2500);
@@ -28,21 +23,21 @@ $("#search_submission").submit(function(e) {
 
 /* Inputting Text */
 $('#keyword').on('input', function() {
-    // do something
-		//console.log('typing...');
     searchit();
 		history.pushState(1, null, $(this).val());
-
    	return false;
-
 });
 
+$('#keyword').blur(function()
+{
+    if( $(this).val().length === 0 ) {
+        $('body').removeClass('display');
+    }
+});
 
 function searchit() {
 // If the field isn't empty do a search
   if ($("#keyword").val() != "" || dirs != "" ){
-
-    //console.log('submitted...');
 
      $('.results').removeClass('dice'); 
      $('.result').removeClass('show');
@@ -60,29 +55,22 @@ function searchit() {
 
       //getJSON
       $.getJSON( "/venues?q="+term, function( data ) {
-
-      // If there are results show them
+     
       if (data.results != ""){
-
         var items = [];
-    
-          //var ul = $('<section>').addClass('results').appendTo('body');
           $(data.results).each(function(index, item) {
             $('.results .inner').append("<div class='result' id='" + item.id + "'><a class='card' href='/venues/"+item.UUID+".html'><h1>" + item.name +"</h1><p><span>"+ item.address1 + "</span><span>" + item.address2 + "</span><span>" + item.city + "</span><span>" + item.region + "</span><span>" + item.postalcode + "</span><span>" + item.country + "</span><span>" + item.url + "</span></p><p class='uuid'><span class='title'>UUID</span><span class='id'>" + item.UUID + "</span></p></a></div>")
           });
 
         }else{
           $('.results .inner').empty();
-          //console.log('no joy');
          $('.results').addClass('dice');
         }
 
       if (!$('body').hasClass('display')){
-        //show results area 
         $('body').addClass('display');
       }
 
-      // delay fade up a little
       setTimeout(function() {
           $('.result').addClass('show');
       }, 100);
