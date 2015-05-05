bf280dc89a763c4cee65b9956def9b24245935dd
Chris Leckie <me@christopherleckie.com>
2015-05-05 11:44:46 -0700

    Card Flip Edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ function searchit() {
       if (data.results != ""){
         var items = [];
           $(data.results).each(function(index, item) {
-            $('.results .inner').append("<div class='result' id='" + item.id + "'><a class='card' target='_blank' href='/venues/"+item.UUID+".html'><h1>" + item.name +"</h1><p><span>"+ item.address1 + "</span><span>" + item.address2 + "</span><span>" + item.city + "</span><span>" + item.region + "</span><span>" + item.postalcode + "</span><span>" + item.country + "</span><span>" + item.url + "</span></p><p class='uuid'><span class='title'>UUID</span><span class='id'>" + item.UUID + "</span></p></a></div>")
+            $('.results .inner').append("<div class='result' id='" + item.id + "'><a class='card' href='/venues/"+item.UUID+".html'><h1>" + item.name +"</h1><p><span>"+ item.address1 + "</span><span>" + item.address2 + "</span><span>" + item.city + "</span><span>" + item.region + "</span><span>" + item.postalcode + "</span><span>" + item.country + "</span><span>" + item.url + "</span></p><p class='uuid'><span class='title'>UUID</span><span class='id'>" + item.UUID + "</span></p></a></div>")
           });
 
         }else{
