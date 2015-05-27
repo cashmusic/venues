6c84ee14e0a9d1c7c01575380626757a3e5bb9a8
Jesse von Doom <jesse@cashmusic.org>
2015-05-27 16:23:17 -0700

    reformatting whitespace and tabs

--- controller.php
+++ controller.php
@@ -9,20 +9,19 @@ session_start();
 require_once('config.php');
 
 try {
-  $db = new PDO($CONNECTION, $USERNAME, $PASSWORD);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $db = new PDO($CONNECTION, $USERNAME, $PASSWORD);
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e) {
-  echo $e->getMessage();
-  exit;
+   echo $e->getMessage();
+   exit;
 }
+
 
 /**************************************************************
 *
 *       Routes and Formats
 *
 **************************************************************/
-// set basic variables
-
 $route = $_REQUEST['p'];
 
 $output_format = 'json';
@@ -46,31 +45,22 @@ if (strpos($route,'.php') !== false) {
 // explode route by '/' and determine what's being asked for
 $exploded_route = explode("/",$route);
 
-
 if (isset($exploded_route[2])) {
-
-  if (count($exploded_route) > 1 && $exploded_route[2] == 'process.php') {
-     $requested_action = 'edited';
-     $UUID = $_POST['UUID'];
-
-
-  } else if (count($exploded_route) > 1 && $exploded_route[1] == 'edit') {
-     $requested_action = 'edit';
-     $UUID = $exploded_route[2];
-
-  }
+   if (count($exploded_route) > 1 && $exploded_route[2] == 'process.php') {
+      $requested_action = 'edited';
+      $UUID = $_POST['UUID'];
+   } else if (count($exploded_route) > 1 && $exploded_route[1] == 'edit') {
+      $requested_action = 'edit';
+      $UUID = $exploded_route[2];
+   }
 } else {
-  if (count($exploded_route) == 1 && $exploded_route[0] == 'venues') {
-     $requested_action = 'search';
-
-  } else if (count($exploded_route) > 1 && $exploded_route[0] == 'venues') {
-     $requested_action = 'details';
-     $UUID = $exploded_route[1];
-
-  }
+   if (count($exploded_route) == 1 && $exploded_route[0] == 'venues') {
+      $requested_action = 'search';
+   } else if (count($exploded_route) > 1 && $exploded_route[0] == 'venues') {
+      $requested_action = 'details';
+      $UUID = $exploded_route[1];
+   }
 }
-
-
 
 
 /*************************************************************
@@ -79,135 +69,94 @@ if (isset($exploded_route[2])) {
 *
 *************************************************************/
 
-// if we found something to do, include needed files and get doing...
-
-
-
 if ($requested_action == 'edited') {
 
+   $venuename = $_POST['venuename'];
+   $address1 = $_POST['address1'];
+   $address2 = $_POST['address2'];
+   $city = $_POST['city'];
+   $region = $_POST['region'];
+   $country = $_POST['country'];
+   $postalcode = $_POST['postalcode'];
+   $latitude = $_POST['latitude'];
+   $longitude = $_POST['longitude'];
+   $url = $_POST['url'];
+   $phone = $_POST['phone'];
+   $type = $_POST['type'];
+   $UUID = $_POST['UUID'];
 
-
-
-    $venuename = $_POST['venuename'];
-    $address1 = $_POST['address1'];
-    $address2 = $_POST['address2'];
-    $city = $_POST['city'];
-    $region = $_POST['region'];
-    $country = $_POST['country'];
-    $postalcode = $_POST['postalcode'];
-    $latitude = $_POST['latitude'];
-    $longitude = $_POST['longitude'];
-    $url = $_POST['url'];
-    $phone = $_POST['phone'];
-    $type = $_POST['type'];
-    $UUID = $_POST['UUID'];
-
-    try {
-
+   try {
       $sql = "UPDATE venues SET name = ?, address1 = ?,address2 = ?,city = ?,region = ?,country = ?,postalcode = ?,latitude = ?, longitude = ?, url = ?,phone = ?,type = ? WHERE UUID = ?";
-
       $q = $db->prepare($sql);
       $q->execute(array($venuename,$address1,$address2,$city,$region,$country,$postalcode,$latitude, $longitude, $url,$phone,$type,$UUID));
 
+   } catch(Exception $e) {
+      echo $e->getMessage();
+      exit;
+   }
+   header('Location: /venues/' . $UUID . '.html');
 
+} else if ($requested_action == 'edit') {
+   // load the venue with the matching UUID for editing
 
+   try {
+      $venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
+      $venuearray->bindParam(1, $UUID);
+      $venuearray->execute();
+   } catch(Exception $e) {
+      echo $e->getMessage();
+      exit;
+   }
+   // load mustache template for edit page
+   $venue = $venuearray->fetch(PDO::FETCH_ASSOC);
+   // output content to browser
+   outputContent($venue,$output_format,'edit');
+} else if ($requested_action == 'details') {
+
+   // load venue with matching UUID, this info is on the specific venue details page
+   try {
+      $venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
+      $venuearray->bindParam(1, $UUID);
+      $venuearray->execute();
+
+   } catch(Exception $e) {
+      echo $e->getMessage();
+      exit;
+   }
+   $venue = $venuearray->fetch(PDO::FETCH_ASSOC);
+   if ($venue) {
+      // output content to browser
+      outputContent($venue,$output_format,'venue');
+   } else {
+      // stuff didn't work!
+      echo "  404 not found!";
+   }
+} else if ($requested_action == 'search') {
+   $name = $_GET['q']; // the search term
+
+   if(isset($name)) {
+      // gets all venues with the search term in the name somewhere
+      try {
+         $searcharray = $db->prepare("SELECT * FROM venues WHERE name LIKE '%" . $name . "%'");
+         $searcharray->execute();
       } catch(Exception $e) {
          echo $e->getMessage();
          exit;
       }
+      $search = $searcharray->fetchALL(PDO::FETCH_ASSOC);
+      $search_results = array("results" => $search, "name" => $name);
 
-  // output content to browser (we're cheating here, assuming success)
-  //outputContent($output_format,'edit');
-  header('Location: /venues/' . $UUID . '.html');
-
-
-  if ($venue) {
-     // output content to browser
-     outputContent($venue,$output_format,'venue');
-  } else {
-     // stuff didn't work!
-     echo " woo 404 not found!";
-  }
-
-} else if ($requested_action == 'edit') {
-  // load the venue with the matching UUID for editing
-
-  try {
-     $venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
-     $venuearray->bindParam(1, $UUID);
-     $venuearray->execute();
-  } catch(Exception $e) {
-     echo $e->getMessage();
-     exit;
-  }
-  // load mustache template for edit page
-  $venue = $venuearray->fetch(PDO::FETCH_ASSOC);
-  // output content to browser
-  outputContent($venue,$output_format,'edit');
-} else if ($requested_action == 'details') {
-
-  // load venue with matching UUID, this info is on the specific venue details page
-  try {
-     $venuearray = $db->prepare('SELECT * FROM venues WHERE UUID = ?');
-     $venuearray->bindParam(1, $UUID);
-     $venuearray->execute();
-
-  } catch(Exception $e) {
-     echo $e->getMessage();
-     exit;
-  }
-  $venue = $venuearray->fetch(PDO::FETCH_ASSOC);
-  if ($venue) {
-     // output content to browser
-     outputContent($venue,$output_format,'venue');
-  } else {
-     // stuff didn't work!
-     echo "  404 not found!";
-  }
-} else if ($requested_action == 'search') {
-
-  $name = $_GET['q']; // the search term
-
-  if(isset($name)) {
-     // gets all venues with the search term in the name somewhere
-     try {
-        $searcharray = $db->prepare("SELECT * FROM venues WHERE name LIKE '%" . $name . "%'");
-        $searcharray->execute();
-     } catch(Exception $e) {
-        echo $e->getMessage();
-        exit;
-     }
-     $search = $searcharray->fetchALL(PDO::FETCH_ASSOC);
-     $search_results = array("results" => $search, "name" => $name);
-     // output content to browser
-
-
-
-     outputContent($search_results,$output_format,'search');
-  }
+      // output content to browser
+      outputContent($search_results,$output_format,'search');
+   }
 } else {
+   if (isset($_SESSION['access_token'])) {
+      $_SESSION['access_token'] = True;
+   } else {
+      $_SESSION['access_token'] = False;
+   }
 
-
-
-
-  //Index page!
-  // loads ALL venues in the db
-  try {
-    $results = $db->query("SELECT * FROM venues");
-  } catch(Exception $e) {
-      echo $e->getMessage();
-      exit;
-  }
-  $venues = $results->fetchAll(PDO::FETCH_ASSOC);
-  // Load mustache template for main page
-
-  if (isset($_SESSION['access_token'])) {
-    $_SESSION['access_token'] = True;
-  } else {
-    $_SESSION['access_token'] = False;
-  }
-
-  outputContent(array("results" => $venues, 'loggedin' => $_SESSION['logged_in']),'html','mainpage');
+   outputContent(array('loggedin' => $_SESSION['logged_in']),'html','mainpage');
 }
 
 
